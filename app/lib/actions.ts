48983c8a3edadccb65b97a22f11d55edc52a73ec
NoOneCoder <alfurqan.shaikh@gmail.com>
2024-12-13 22:17:40 +0530

    error resolved.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -101,7 +101,5 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
-  } catch {
-    return { message: "Database Error: Failed to Delete Invoice." };
-  }
+  } catch {}
 }
